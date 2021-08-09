9ad91635379354925b2481fe366a73a5e2f7a5ab
RayWarthog <choulei0112@hotmail.com>
2021-08-10 03:10:52 +0800

    Messages page

---
+++ src/views/Messages.vue
@@ -0,0 +1,15 @@
+<template>
+  <Page id="messages" title_src="gif/messages.gif" title_alt="Messages" back_link="/menu" back_btn_type="3" sky_bg large_title>
+
+  </Page>
+</template>
+
+<script>
+import Page from '@/components/Page.vue'
+
+export default {
+  components: {
+    Page
+  }
+}
+</script>
