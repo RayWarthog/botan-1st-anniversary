57788ede45df5001848dbb2b04f09ec5993bca7f
RayWarthog <choulei0112@hotmail.com>
2021-08-13 09:52:48 +0800

    Remember language selection, default to japanese if detected

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,31 @@
 import { reactive } from 'vue'
 
+const userLanguages = window.navigator.languages || [window.navigator.language || window.navigator.userLanguage]
+const userLangPref = localStorage.getItem('lang')
+let defaultLangauge = 'en'
+
+if (userLangPref === 'ja' || userLangPref === 'en') {
+  defaultLangauge = userLangPref
+} else {
+  if (userLanguages.includes('ja')) {
+    defaultLangauge = 'ja'
+  }
+}
+
 const store = {
   state: reactive({
-    lang: 'en'
+    lang: defaultLangauge
   }),
 
-  toggleLang () {
-    if (this.state.lang === 'ja') {
+  toggleLang (lang = null) {
+    if (lang) {
+      this.state.lang = lang
+    } else if (this.state.lang === 'ja') {
       this.state.lang = 'en'
     } else {
       this.state.lang = 'ja'
     }
+    localStorage.setItem('lang', this.state.lang)
   }
 }
 
